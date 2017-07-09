71c6f4dc4c74b7a2ad240f220fa27067f11afea2
alchemyfordummies <frobergtd@gmail.com>
2017-07-08 22:51:53 -0400

    Initial commit

---
+++ app/src/main/java/com/example/timothy/physicsgame/MainMenuBackgroundView.java
@@ -0,0 +1,44 @@
+package com.example.timothy.physicsgame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import java.util.Random;
+
+public class MainMenuBackgroundView extends View {
+
+    private Paint _easel    = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Random _random  = new Random();
+
+
+    public MainMenuBackgroundView(Context context) {
+        super(context);
+        init(null, 0);
+    }
+
+    public MainMenuBackgroundView(Context context, AttributeSet attributes) {
+        super(context, attributes);
+        init(attributes, 0);
+    }
+
+    public MainMenuBackgroundView(Context context, AttributeSet attributes, int defStyle) {
+        super(context, attributes);
+        init(attributes, defStyle);
+    }
+
+    public void init(AttributeSet attrs, int defStyle) {
+        _easel.setColor(Color.BLUE);
+    }
+
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        int xCoordinate = _random.nextInt(getWidth());
+        int yCoordinate = _random.nextInt(getHeight());
+        canvas.drawLine(0, 0, xCoordinate, yCoordinate, _easel);
+    }
+}
